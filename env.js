c0bce7d434cbd98a38f8f7d87a67d3880c8deff4
Dan Potter <dpotter@mitre.org>
2020-04-28 11:47:59 -0400

    Fix lint errors

--- env.js
+++ env.js
@@ -1,15 +1,15 @@
 class configuration {
-	 constructor(){
-		this.TRIALSCOPE_ENDPOINT = "https://clinicaltrialconnect.dev/graphql";
-		this.PORT = 3000;
-		this.TOKEN = process.env.TRIALSCOPE_TOKEN;
-	 }
-   defaultEnvObject(){
-	   return {
-			port : this.PORT,
-			token : this.TOKEN,
-			trialscope_endpoint : this.TRIALSCOPE_ENDPOINT
-		}
-   }
+  constructor() {
+    this.TRIALSCOPE_ENDPOINT = 'https://clinicaltrialconnect.dev/graphql';
+    this.PORT = 3000;
+    this.TOKEN = process.env.TRIALSCOPE_TOKEN;
+  }
+  defaultEnvObject() {
+    return {
+      port : this.PORT,
+      token : this.TOKEN,
+      trialscope_endpoint : this.TRIALSCOPE_ENDPOINT
+    };
+  }
 }
 module.exports = configuration;
--- server.js
+++ server.js
@@ -1,69 +1,63 @@
-var mapping = require('./mapping');
+const mapping = require('./mapping'),
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  config = require('./env.js'),
+  fetch = require('node-fetch');
 
-try{
-	var express = require('express'),
-		app = express(),
-		bodyParser = require('body-parser'),
-		config = require('./env.js'),
-		enviroment = new config().defaultEnvObject(),
-		fetch = require('node-fetch');
-		app.use(bodyParser.json());
-		
-		app.use(function (req, res, next) {
-   
-         // Website you wish to allow to connect
-         res.setHeader('Access-Control-Allow-Origin', '*');
-     
-         // Request methods you wish to allow
-         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-     
-         // Request headers you wish to allow
-         res.setHeader('Access-Control-Allow-Headers', 'Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With, WU-Api-Key, WU-Api-Secret');
-         
-         next();
-      });
-      
-     
-	  
-	  /* Default call*/
-      app.get('/', function(req, res) {
-			   res.status(200).send("Hello from Clinical Trial");
-       });
+const app = express(),
+  enviroment = new config().defaultEnvObject();
 
-      /* get trialscope conditions (str) list from code (str) list */
-      app.post('/getConditions', function(req, res) {
-                var codeList = req.body;
-                var conditions = mapping.mapConditions(codeList);
-                var result = JSON.stringify(Array.from(conditions));
-                res.status(200).send(result);
-      });
-	   
-	   /* get clinical trial results*/
-      app.post('/getClinicalTrial', function(req, res) {
-				var myHeaders = new fetch.Headers;
-				myHeaders.append("Content-Type", "application/json");
-				myHeaders.append("Authorization", "Bearer "+enviroment.token);
-				
-				var raw = JSON.stringify({"query": req.body.inputParam});
+app.use(bodyParser.json());
 
-				var requestOptions = {
-				  method: 'POST',
-				  headers: myHeaders,
-				  body: raw,
-				  redirect: 'follow'
-				};
-				
-				fetch(enviroment.trialscope_endpoint, requestOptions)
-				  .then(response => response.text())
-				  .then(result => {
-					  res.status(200).send(result);
-				  })
-				  .catch(error =>{
-					  res.status(400).send(error);
-				  });
-       });
-	  
-	  app.listen(enviroment.port)
-}
-catch(e){
-}
+app.use(function(_req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With, WU-Api-Key, WU-Api-Secret');
+
+  next();
+});
+
+/* Default call*/
+app.get('/', function(_req, res) {
+  res.status(200).send('Hello from Clinical Trial');
+});
+
+/* get trialscope conditions (str) list from code (str) list */
+app.post('/getConditions', function(req, res) {
+  let codeList = req.body;
+  let conditions = mapping.mapConditions(codeList);
+  let result = JSON.stringify(Array.from(conditions));
+  res.status(200).send(result);
+});
+
+/* get clinical trial results*/
+app.post('/getClinicalTrial', function(req, res) {
+  const myHeaders = new fetch.Headers;
+  myHeaders.append('Content-Type', 'application/json');
+  myHeaders.append('Authorization', 'Bearer ' + enviroment.token);
+
+  const raw = JSON.stringify({query: req.body.inputParam});
+
+  const requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+  };
+
+  fetch(enviroment.trialscope_endpoint, requestOptions)
+    .then(response => response.text())
+    .then(result => {
+      res.status(200).send(result);
+    })
+    .catch(error =>{
+      res.status(400).send(error);
+    });
+});
+
+app.listen(enviroment.port);
