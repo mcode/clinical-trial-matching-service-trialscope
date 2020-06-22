b0ed9e95d2993512b555254fcdb6e7e3e5c13690
Nikhil Gaddam <39496206+19ngaddam@users.noreply.github.com>
2020-06-22 13:58:38 -0400

    Added several tests, fixed bad request errors, fixed tsconfig settings

--- spec/support/server.spec.ts
+++ spec/support/server.spec.ts
@@ -1,5 +1,5 @@
+import e from "express";
 
-import "jasmine";
 const request = require("supertest");
 let server = require("../../src/server");
 
@@ -15,6 +15,43 @@ describe("server tests", () => {
             .expect(200, done);
 
     });
+
+    it("responds to / with hello from clinical trial ", () => {
+        return request(server)
+            .get('/')
+            .set('Accept', 'application/json')
+            .expect(200)
+            .then((res) => {
+                expect(res.text).toBe("Hello from Clinical Trial");
+            });
+
+
+    });
+    it("responds to /getConditions ", () => {
+        return request(server)
+            .post('/getConditions')
+            .set('Accept', 'application/json')
+            .expect(200);
+
+    });
+
+    it("responds to /getClinicalTrial with improper patient bundle ", () => {
+        return request(server)
+            .post('/getClinicalTrial')
+            .send({ patientData: {} })
+            .set('Accept', 'application/json')
+            .expect(500);
+
+    });
+    it("responds to /getClinicalTrial with no patientBundle param  ", () => {
+        return request(server)
+            .post('/getClinicalTrial')
+            .send({})
+            .set('Accept', 'application/json')
+            .expect(400);
+
+    });
+
 });
 
 
