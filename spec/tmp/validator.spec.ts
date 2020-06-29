7a6a7d0dbda2d7f03af8c4c99b9d15768845c877
Nikhil Gaddam <39496206+19ngaddam@users.noreply.github.com>
2020-06-29 13:25:00 -0400

    Moved fhir validator into a unit test , and documented usage

---
+++ spec/tmp/validator.spec.ts
@@ -0,0 +1,29 @@
+/*Use the fhir validator jar to check the researchstudy /bundle being sent to the UI is formatted properly and satisfy fhir standards
+
+Download the fhir validator here: https://storage.googleapis.com/ig-build/org.hl7.fhir.validator.jar and place in this directory
+
+Paste an example research study in the resource.json file before running the test
+
+*/
+describe("Fhir Validator jar", () => {
+
+    /*This test case is temporarily disabled. Change xit --> it to run this test case.
+      Note: The test must be disabled (changed to xit) on commit 
+    */
+
+
+    xit("validates sample fhir object" , () => {
+
+        var exec = require('child_process').exec;
+
+
+        var child = exec('java -jar ./org.hl7.fhir.validator.jar ./resource.json', function (error, stdout, stderr) { //standard output of jar file is through stdout
+            console.log('Output -> ' + stdout);
+            if (error !== null) {
+            console.log("Error -> " + error);
+            }
+        });
+
+    });
+
+});
