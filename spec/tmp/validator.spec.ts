1a0e20efc8f37376cfd1f9952ffa75b4de487761
Nikhil Gaddam <39496206+19ngaddam@users.noreply.github.com>
2020-06-30 10:58:21 -0400

    fixed test case for fhir validator to support blocking

--- spec/tmp/validator.spec.ts
+++ spec/tmp/validator.spec.ts
@@ -6,7 +6,10 @@ Paste an example research study in the resource.json file before running the tes
 
 */
 
+import { exec } from "child_process";
+
 //NOTE: The jar file must be named org.hl7.fhir.validator.jar
+
 
 
 
@@ -15,20 +18,29 @@ describe("Fhir Validator jar", () => {
     /*This test case is temporarily disabled. Change xit --> it to run this test case.
       NOTE: The test must be disabled (changed to xit) on commit 
     */
+   
+   beforeEach(function() {
+       
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
+    });
+    afterEach(function(){
+        jasmine.DEFAULT_TIMEOUT_INTERVAL=5000;
+    });
 
+    xit("validates sample fhir object" ,   function (done) {
 
-    xit("validates sample fhir object" , () => {
-
-        var exec = require('child_process').exec;
-
-
-        var child = exec('java -jar ./org.hl7.fhir.validator.jar ./resource.json', function (error, stdout, stderr) { //standard output of jar file is through stdout
-            console.log('Output -> ' + stdout);
-            if (error !== null) {
-            console.log("Error -> " + error);
-            }
-        });
-
+            const child=   exec('java -jar ./spec/tmp/org.hl7.fhir.validator.jar ./spec/tmp/resource.json', function (error, stdout,stderr) { //standard output of jar file is through stdout
+                console.log(`Output -> ${stdout}`);
+                if (error !== null) {
+                    console.log(`Error ->  ${error}`);
+            
+                }
+        
+                done();
+            }); 
+       
+       
+    
     });
 
 });
