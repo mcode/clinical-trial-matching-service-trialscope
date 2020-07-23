91b07192426f3b10dce4293e77a4c07ff95ff88f
Nikhil Gaddam <39496206+19ngaddam@users.noreply.github.com>
2020-07-23 09:46:58 -0400

    made validation test set travis ready

--- spec/tmp/validator.spec.ts
+++ spec/tmp/validator.spec.ts
@@ -12,9 +12,7 @@ import { exec } from "child_process";
 
 describe("Fhir Validator jar", () => {
 
-    /*This test case is temporarily disabled. Change xit --> it to run this test case.
-      NOTE: The test must be disabled (changed to xit) on commit 
-    */
+    
    beforeEach(function() {
        
         jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
@@ -22,7 +20,7 @@ describe("Fhir Validator jar", () => {
     afterEach(function(){
         jasmine.DEFAULT_TIMEOUT_INTERVAL=5000;
     });
-    xit("validates sample fhir object" , function (done) {
+    it("validates sample fhir object" , function (done) {
 
         const child=   exec('java -jar ./spec/tmp/org.hl7.fhir.validator.jar ./spec/tmp/resource.json', function (error, stdout,stderr) { //standard output of jar file is through stdout
             console.log(`Output -> ${stdout}`);
